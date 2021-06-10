c9a7fbb13340897e8f032597068253070aadc672
gamersindo1223 <76719536+gamersindo1223@users.noreply.github.com>
2021-06-10 08:36:39 +0700

    lol

--- index.js
+++ index.js
@@ -2,34 +2,6 @@ const express = require('express')
 const Discord = require('discord.js')
 const path = require('path')
 
-const html = `<!DOCTYPE html>
-
-<html>
-    <body>
-        <form method="POST" action="http://localhost:3000/form">
-            <form>
-                <label for="first-name">First name:</label><br>
-                <input type="text" id="first-name" name="first-name"><br>
-                <label for="last-name">Last name:</label><br>
-                <input type="text" id="last-name" name="last-name"><br>
-                <label for="discord usertag">discord usertag:</label><br>
-                <input type="text" id="discord usertag" name="discord usertag">
-                <input type="submit">
-              </form>
-        </form>
-    </body>
-</html>`
-
-async function handleRequest(request) {
-  return new Response(html, {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-    },
-  })
-}
-addEventListener("fetch", event => {
-    return event.respondWith(handleRequest(event.request))
-  })
 const app = express()
 //https://discord.com/api/webhooks//
 const webhook = new Discord.WebhookClient("851984962760081419", "lUFWWdJu8PPxyyuIhpw2iBzyiHtOYFj-LJYLMxKgnQjUIHqBwGQ-Rkq6RW7TQrMug34N")
